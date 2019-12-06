fd78c1d9433ef2a193f37ed524888fa4bd1b7c67
Lev Simionenko <levsim2016@gmail.com>
2019-12-06 23:18:17 +0300

    change card component to use styles

--- src/ui-components/components/card/Card.tsx
+++ src/ui-components/components/card/Card.tsx
@@ -1,10 +1,20 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, HTMLAttributes } from 'react';
 import styles from './card.module.css';
 
-export class Card extends React.PureComponent {
+export class Card extends React.PureComponent<HTMLAttributes<HTMLDivElement>> {
+    private getCardClasses(): string {
+        if (this.props.className === undefined) {
+            return styles.card;
+        }
+
+        return `${styles.card} ${this.props.className}`;
+    }
+
     public render(): ReactNode {
+        const cardClasses = this.getCardClasses();
+
         return (
-            <div className={styles.card}>
+            <div className={cardClasses}>
                 {this.props.children}
             </div>
         );
