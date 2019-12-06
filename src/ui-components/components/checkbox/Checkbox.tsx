d961693d8a018c39eac68c547a32d46e5794eff1
Lev Simionenko <levsim2016@gmail.com>
2019-12-06 23:26:49 +0300

    add strikethrough text for checkbox

--- src/ui-components/components/checkbox/Checkbox.tsx
+++ src/ui-components/components/checkbox/Checkbox.tsx
@@ -5,21 +5,21 @@ import checkboxMarker from '../../../assets/images/checkbox-marker.svg';
 import { ICheckboxProps } from '../../interfaces/ICheckboxProps';
 
 export class Checkbox extends React.PureComponent<ICheckboxProps> {
-    private getMarkerClasses(): string {
+    private getCheckboxClasses(): string {
         if (this.props.isChecked === true) {
-            return `${styles.mark} ${styles.checked}`;
+            return `${styles.checkbox} ${styles.checked}`;
         }
 
-        return styles.mark;
+        return styles.checkbox;
     }
 
     public render(): ReactNode {
         const { label } = this.props;
-        const checkboxMarkerClasses = this.getMarkerClasses();
+        const checkboxClasses = this.getCheckboxClasses();
 
         return (
-            <div className={styles.checkbox}>
-                <div className={checkboxMarkerClasses}>
+            <div className={checkboxClasses}>
+                <div className={styles.marker}>
                     <img src={checkboxMarker} alt="Checkbox marker" />
                 </div>
                 <span className={styles.label}>
