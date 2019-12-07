6bbc7406713293bc1b7bd7c9582200836f7ef713
Lev Simionenko <levsim2016@gmail.com>
2019-12-07 05:16:27 +0300

    fix layout height styles

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -4,13 +4,19 @@ import { mockedTasks } from '../../mocks/tasks';
 
 import { AddTaskPanel } from '../add-task-panel/AddTaskPanel';
 import { TaskList } from '../task-list/TaskList';
+import { Header } from '../header/Header';
+import { Footer } from '../footer/Footer';
 
 export class Layout extends React.PureComponent {
     public render(): ReactNode {
         return (
-            <div className={styles.wrapper}>
-                <AddTaskPanel></AddTaskPanel>
-                <TaskList tasks={mockedTasks}></TaskList>
+            <div className={styles.layout}>
+                <Header></Header>
+                <main className={styles.wrapper}>
+                    <AddTaskPanel></AddTaskPanel>
+                    <TaskList tasks={mockedTasks}></TaskList>
+                </main>
+                <Footer></Footer>
             </div>
         );
     }
