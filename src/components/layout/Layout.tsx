2a64c673631b7fc2ad2ee148ac3b2ef230576036
Lev Simionenko <levsim2016@gmail.com>
2019-12-08 17:43:25 +0300

    use test calendar

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -6,12 +6,19 @@ import { AddTaskPanel } from '../add-task-panel/AddTaskPanel';
 import { TaskList } from '../task-list/TaskList';
 import { Header } from '../header/Header';
 import { Footer } from '../footer/Footer';
+import { Calendar } from '../../ui-components';
 
 export class Layout extends React.PureComponent {
     public render(): ReactNode {
         return (
             <div className={styles.layout}>
                 <Header></Header>
+                <section>
+                    <Calendar 
+                        month={12} 
+                        selectedDay={8}
+                    ></Calendar>
+                </section>
                 <main className={styles.contentContainer}>
                     <AddTaskPanel></AddTaskPanel>
                     <TaskList tasks={mockedTasks}></TaskList>
